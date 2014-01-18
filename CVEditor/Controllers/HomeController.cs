aa5f666af2ebfbbc3f4470ff45f9af9b620da00e
nikolay-spasov <nikolay.spasov.bg@gmail.com>
2014-01-18 12:19:44 +0200

    initial

---
+++ CVEditor/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using CVEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CVEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostCV(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                string fileId = Guid.NewGuid().ToString();
+                var path = FileIdToPath(fileId);
+                file.SaveAs(path);
+
+                return RedirectToAction("ShowCV", new { id = fileId });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private string FileIdToPath(string fileId)
+        {
+            return Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileId + ".xml");
+        }
+
+        public ViewResult ShowCV(string id)
+        {
+            CV cv = LoadFromXml(id);
+            TempData["fileid"] = id;
+            return View(cv);
+        }
+
+        public ActionResult EditCV(string id)
+        {
+            CV cv = LoadFromXml(id);
+            TempData["fileid"] = id;
+
+            return View(cv);
+        }
+
+        [HttpPost]
+        public ActionResult EditCV(CV cv, string id)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = FileIdToPath(id);
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    XmlSerializer ser = new XmlSerializer(cv.GetType());
+                    ser.Serialize(writer.BaseStream, cv);
+                }
+                return RedirectToAction("ShowCV", new { id = id });
+            }
+            else
+            {
+                TempData["fileid"] = id;
+                return View(cv);
+            }
+        }
+
+        private CV LoadFromXml(string fileId)
+        {
+            var path = FileIdToPath(fileId);
+
+            CV cv = new CV();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                XmlSerializer serializer = new XmlSerializer(cv.GetType());
+                cv = serializer.Deserialize(reader.BaseStream) as CV;
+            }
+
+            return cv;
+        }
+
+        public ActionResult AddSlot(string id)
+        {
+            CV cv = LoadFromXml(id);
+            cv.Jobs.Add(new Job
+            {
+                From = DateTime.Now,
+                To = DateTime.Now
+            });
+
+            var path = FileIdToPath(id);
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                XmlSerializer ser = new XmlSerializer(cv.GetType());
+                ser.Serialize(writer.BaseStream, cv);
+            }
+
+            return RedirectToAction("EditCV", new { id = id });
+        }
+
+        public FileResult DownloadCV(string id)
+        {
+            var path = FileIdToPath(id);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, "CV.xml");
+        }
+    }
+}
---
+++ CVEditor/Models/CV.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CVEditor.Models
+{
+    public class CV
+    {
+        [Required(ErrorMessage = "'First name' is required")]
+        [Display(Name = "First name: ")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "'Last name' is required")]
+        [Display(Name = "Last name: ")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address: ")]
+        public string Address { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number: ")]
+        [Required(ErrorMessage = "'Phone number' is required")]
+        public string PhoneNumber { get; set; }
+
+        public List<Job> Jobs { get; set; }
+    }
+
+    public class Job
+    {
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "'From' is required")]
+        [Display(Name = "From :")]
+        public DateTime From { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "'To' is required")]
+        [Display(Name = "To :")]
+        public DateTime To { get; set; }
+        public string Company { get; set; }
+
+        [Required(ErrorMessage = "'Job Title' is required")]
+        [Display(Name = "Job Title: ")]
+        public string JobTitle { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "'Detailed information' is required")]
+        [Display(Name = "Detailed information: ")]
+        public string DetailedInformation { get; set; }
+    }
+}
